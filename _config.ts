0fa361c52d871398538b9a95787a8fb86b26acbc
Lidessen <ztytotoro@outlook.com>
2025-02-27 16:22:53 +0800

    update

--- _config.ts
+++ _config.ts
@@ -1,6 +1,6 @@
 import lume from "lume/mod.ts";
 import favicon from "lume/plugins/favicon.ts";
-import blog from "https://deno.land/x/lume_theme_simple_blog@v0.13.0/mod.ts";
+import blog from "https://deno.land/x/lume_theme_simple_blog@v0.15.6/mod.ts";
 
 const site = lume();
 
